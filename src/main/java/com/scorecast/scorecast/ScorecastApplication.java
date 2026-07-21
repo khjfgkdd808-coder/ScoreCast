84c9cdb5adfdd5c179db3019a2a86c535143a2a4
khjfgkdd808-coder <khjfgkdd123@naver.com>
2026-07-21 23:33:01 +0900

    feat: initialize Spring Boot project

---
+++ src/main/java/com/scorecast/scorecast/ScorecastApplication.java
@@ -0,0 +1,13 @@
+package com.scorecast.scorecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorecastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scorecast/scorecast/ScorecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorecast.scorecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
